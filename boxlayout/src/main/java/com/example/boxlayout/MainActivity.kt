e52ac7c3e9e63b46f151da905ac49911d06a07f7
power <x.oybek94@gmail.com>
2023-09-22 10:51:47 +0900

    BoxLayout

--- boxlayout/src/main/java/com/example/boxlayout/MainActivity.kt
+++ boxlayout/src/main/java/com/example/boxlayout/MainActivity.kt
@@ -1,12 +1,10 @@
-package com.example.rowcolumn
+package com.example.boxlayout
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,7 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.rowcolumn.ui.theme.Compose_BasicsTheme
+import com.example.boxlayout.ui.theme.Compose_BasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,60 +33,50 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    MainScreen()
                 }
             }
         }
     }
 }
-
 @Composable
 fun MainScreen(){
-    Column(horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Row{
-            Column {
-                TextCell(text = "1")
-                TextCell(text = "2")
-                TextCell(text = "3")
-            }
-            Column {
-                TextCell(text = "4")
-                TextCell(text = "5")
-                TextCell(text = "6")
-            }
-            Column {
-                TextCell(text = "7")
-                TextCell(text = "8")
-            }
-        }
-        Row {
-           TextCell(text = "9")
-           TextCell(text = "10")
-           TextCell(text = "11")
-        }
+    Box(contentAlignment = Alignment.CenterEnd,
+    modifier = Modifier.size(400.dp,400.dp)){
+        val height = 200.dp
+        val width = 200.dp
+
+        TextCell(text = "1",Modifier.size(width = width,height = height).align(Alignment.TopStart))
+        TextCell(text = "2",Modifier.size(width = width,height = height).align(Alignment.BottomStart))
+        TextCell(text = "3",Modifier.size(width = width,height = height).align(Alignment.CenterEnd))
     }
 }
 
 @Composable
-fun TextCell(text:String,modifier:Modifier = Modifier){
+fun TextCell(text:String,modifier: Modifier = Modifier,fontSize:Int = 150){
     val cellModifier = Modifier
         .padding(4.dp)
-        .size(100.dp, 100.dp)
-        .border(width = 4.dp, color = Color.Green)
+        .border(width = 5.dp, color = Color.Green)
 
-    Text(text = text,
-    cellModifier.then(modifier),
-    fontSize = 70.sp,
-    fontWeight = FontWeight.Bold,
+    Text(text = text,cellModifier.then(modifier),
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold,
         textAlign = TextAlign.Center
-    )
+        )
+
+    Surface {
+        Text(text = text,cellModifier.then(modifier),
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_BasicsTheme {
-         MainScreen()
+       MainScreen()
     }
 }
